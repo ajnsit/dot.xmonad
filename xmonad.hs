6cd5602eb9ee961c032609645876daae6c44156a
Anupam Jain <ajnsit@gmail.com>
2012-11-07 03:38:03 +0530

    Added a "magnify current window" mode, quickly toggleable by M-m. Use M-minus and M-plus to decrease and increase magnification.

--- xmonad.hs
+++ xmonad.hs
@@ -10,6 +10,8 @@ import XMonad.Layout.NoBorders (smartBorders, noBorders)
 import XMonad.Layout.Tabbed (tabbed)
 import XMonad.Layout.Decoration (shrinkText, defaultTheme, activeBorderColor, activeColor, activeTextColor, inactiveBorderColor, inactiveColor, inactiveTextColor, decoHeight)
 import XMonad.Layout.Fullscreen (fullscreenFull)
+import XMonad.Layout.Circle (Circle(..))
+import XMonad.Layout.Magnifier (magnifier, MagnifyMsg(..))
 --import XMonad.Layout.Spiral (spiral)
 import XMonad.Util.EZConfig (additionalKeysP, removeKeysP)
 import qualified XMonad.StackSet as W
@@ -77,12 +79,13 @@ myLayout =
    ||| rename "Mirror"     ( Mirror (Tall 1 (3/100) (1/2))   )
    ||| rename "Tabbed"     ( tabbed shrinkText myTabColors   )
    ||| rename "Maximised"  ( Full                            )
+   ||| rename "Circle"     ( Circle                          )
    -- ||| rename "Spiral"     ( spiral (6/7)                    )
    )
   -- These effectively take over the entire screen
   |||  rename "Fullscreen" ( noBorders (fullscreenFull Full) )
   where
-    rename s = renamed [Replace s]
+    rename s = renamed [Replace s] . magnifier
 
 
 ------------------------------------------------------------------------
@@ -111,6 +114,7 @@ myKeys = concat
     , ( "M-S-t",    sendMessage $ JumpToLayout "Tabbed"     )
     , ( "M-S-l",    sendMessage $ JumpToLayout "Tall"       )
     , ( "M-S-m",    sendMessage $ JumpToLayout "Mirror"     )
+    , ( "M-S-r",    sendMessage $ JumpToLayout "Circle"     )
     ],
     -- Use the dmenu shortcut for Synapse
     [ ( "M-p",      spawn "exec synapse"                    )
@@ -119,6 +123,14 @@ myKeys = concat
     -- Need to use a custom action with gnome-terminal because it does not
     -- support -name attribute (uses --name instead)
     [ ( "M-t",      scratchpadSpawnActionCustom "gnome-terminal --disable-factory --name scratchpad" )
+    ],
+    -- Layout - Current window magnifier key bindings
+    [ ("M-=",   sendMessage MagnifyMore)
+    , ("M--",   sendMessage MagnifyLess)
+    , ("M-o",   sendMessage ToggleOff  )
+    , ("M-S-o", sendMessage ToggleOn   )
+    -- We reuse the same binding as "switch focus to master"
+    , ("M-m",   sendMessage Toggle     )
     ]
   ]
 
@@ -127,6 +139,9 @@ myUnKeys =
     -- Remove default dmenu binding to allow overriding
     -- (already changed by gnome-config to gnome-run command)
     [ "M-p"
+    -- Remove "switch focus to master"
+    -- I never use it. And I needed the key for window magnify toggle
+    , "M-m"
     ]
 
 ------------------------------------------------------------------------
